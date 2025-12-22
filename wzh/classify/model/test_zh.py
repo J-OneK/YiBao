d0240974ae5947382d23aef64940a19341a76644
nnnewu <w1250235092@outlook.com>
2025-12-22 10:14:04 +0800

    classify

--- wzh/classify/model/test_zh.py
+++ wzh/classify/model/test_zh.py
@@ -3,10 +3,10 @@ from FlagEmbedding import FlagAutoModel
 model = FlagAutoModel.from_finetuned('BAAI/bge-base-zh-v1.5',
                                       query_instruction_for_retrieval="Represent this sentence for searching relevant passages:",
                                       use_fp16=True)
-water = ["水路运输", "海运", "航运"]
-air = ["航空运输", "空运"]
-land = ["铁路运输", "陆路运输"]
-input = ["by sea"]
+water = ["水路运输"]
+air = ["航空运输"]
+land = ["铁路运输"]
+input = ["水运"]
 
 emb_water = model.encode(water)
 emb_air = model.encode(air)
@@ -21,6 +21,6 @@ similarity_air = mean_air @ input_emb.T
 similarity_water = mean_water @ input_emb.T
 similarity_land = mean_land @ input_emb.T
 
-print(similarity_air)
 print(similarity_water)
+print(similarity_air)
 print(similarity_land)
